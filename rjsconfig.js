af80ddea53be002ddd644c13a421ae64bdbcf93b
Jim Boswell <jimb@yellcorp.org>
2013-05-15 23:11:29 -0700

    adding json2 - extendscript lacks JSON global

--- rjsconfig.js
+++ rjsconfig.js
@@ -3,11 +3,15 @@
 	paths: {
 		requireLib: "../node_modules/requirejs/require",
 		underscore: "../lib/underscore/underscore",
+		json:       "../lib/json/json2",
 		es:         "../lib/es"
 	},
 	shim: {
 		underscore: {
 			exports: "_"
+		},
+		json: {
+			exports: "JSON"
 		}
 	},
 	include: [ "requireLib" ],
