614042484243819d11370cd3290251fccbbd37c5
Jim Boswell <jimb@yellcorp.org>
2013-09-13 20:06:42 +0100

    proper...ish build script

--- build.sh
+++ build.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
-mkdir -p build/ps
-./node_modules/.bin/r.js \
-	-o rjsconfig.js \
-	name=ps/spritestrip \
-	out=build/ps/spritestrip.jsx
+
+src=src
+out=build
+
+for js in $src/{ae,ps}/*.js
+do
+	barename=${js%.js}
+	barename=${barename#$src/}
+	outfile=$out/$barename.jsx
+	dirname=$(dirname $outfile)
+	mkdir -vp $dirname
+	./node_modules/.bin/r.js \
+		-o rjsconfig.js \
+		name=$barename \
+		out=$out/$barename.jsx
+done
