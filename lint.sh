e083dde37fad1ebbbdd8d1233e10cd17f3a28e52
Jim Boswell <boswell@soapcreative.com>
2014-04-01 18:18:54 -0700

    switch to jshint

--- lint.sh
+++ lint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+# npm install jshint
+JSHINT=node_modules/jshint/bin/jshint
+
 for basedir in src lib/es
 do
-	find $basedir -iname "*.js" -print0 | \
-		xargs -0 jslint --white --plusplus --nomen
+	find $basedir -iname "*.js" \
+		-print0 | xargs -0 $JSHINT --config=jshintconfig.json
 done
